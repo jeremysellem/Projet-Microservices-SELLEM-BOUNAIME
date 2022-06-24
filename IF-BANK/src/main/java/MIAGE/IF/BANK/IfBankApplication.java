7f5474ca8289be4db10cf1b150b8f173dfffcc5c
Mohamed Bounaime <mohamed.bounaime@nrgybox.com>
2022-06-24 16:33:18 +0200

    first commit

---
+++ IF-BANK/src/main/java/MIAGE/IF/BANK/IfBankApplication.java
@@ -0,0 +1,13 @@
+package MIAGE.IF.BANK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfBankApplication.class, args);
+	}
+
+}
---
+++ IF-BANK/src/test/java/MIAGE/IF/BANK/IfBankApplicationTests.java
@@ -0,0 +1,13 @@
+package MIAGE.IF.BANK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
